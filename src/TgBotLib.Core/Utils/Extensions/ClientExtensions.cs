a815bdaebb8e047169e3b450e608829b539dc896
Sasha Ushankov <sasha.ushankov@gmail.com>
2024-04-13 00:44:21 +0300

    Fixed escape

--- src/TgBotLib.Core/Utils/Extensions/ClientExtensions.cs
+++ src/TgBotLib.Core/Utils/Extensions/ClientExtensions.cs
@@ -37,7 +37,30 @@ public static class ClientExtensions
 
     public static string EscapeMarkdownSpecialCharacters(this string input)
     {
-        char[] specialCharacters = ['\\', '*', '_', '[', ']', '(', ')', '!', '#', '.'];
+        char[] specialCharacters =
+        [
+            '\\',
+            '_',
+            '*',
+            '[',
+            ']',
+            '(',
+            ')',
+            '~',
+            '`',
+            '>',
+            '<',
+            '&',
+            '#',
+            '+',
+            '-',
+            '=',
+            '|',
+            '{',
+            '}',
+            '.',
+            '!'
+        ];
 
         foreach (var specialChar in specialCharacters)
         {
