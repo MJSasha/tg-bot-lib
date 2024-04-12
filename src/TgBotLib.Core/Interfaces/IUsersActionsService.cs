6cc60f75790fea9c73e8ca30b61fc114bb3c534c
Sasha Ushankov <sasha.ushankov@gmail.com>
2024-04-12 21:15:07 +0300

    Minor fixes

--- src/TgBotLib.Core/Interfaces/IUsersActionsService.cs
+++ src/TgBotLib.Core/Interfaces/IUsersActionsService.cs
@@ -4,7 +4,7 @@ namespace TgBotLib.Core;
 
 public interface IUsersActionsService
 {
-    void HandleUser(long chatId, string actionName, object model);
+    void HandleUser(long chatId, string actionName, object payload = null);
     UserActionStepInfo? GetUserActionStepInfo(long chatId);
     void IncrementStep(long chatId);
     void RemoveUser(long chatId);
