0e624945ff54abd25a38077ab8cd4446e6bf5501
VVSaltykov <vitya.saltyckov@gmail.com>
2024-04-12 21:02:03 +0300

    add payload

--- src/TgBotLib.Core/Models/UserActionStepInfo.cs
+++ src/TgBotLib.Core/Models/UserActionStepInfo.cs
@@ -4,16 +4,23 @@ public class UserActionStepInfo
 {
     public int Step { get; private set; }
     public string ActionName { get; private set; }
+    private object Model { get; set; }
 
-    public UserActionStepInfo(string actionName)
+    public UserActionStepInfo(string actionName, object model)
     {
         Step = 0;
         ActionName = actionName;
+        Model = model;
     }
 
     public UserActionStepInfo IncrementStep()
     {
         Step++;
         return this;
+    }
+
+    public T GetPayload<T>()
+    {
+        return (T)Model;
     }
 }
