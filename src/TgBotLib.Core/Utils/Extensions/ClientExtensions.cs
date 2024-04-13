d603da4bdb1a345fbdfbf2361f393ade4c27138b
Sasha Ushankov <sasha.ushankov@gmail.com>
2024-04-13 22:55:44 +0300

    Added some utility

--- src/TgBotLib.Core/Utils/Extensions/ClientExtensions.cs
+++ src/TgBotLib.Core/Utils/Extensions/ClientExtensions.cs
@@ -9,7 +9,8 @@ public static class ClientExtensions
 {
     public static char[] ChartsForEscape { get; set; } = ['[', ']', '(', ')', '~', '>', '<', '&', '#', '+', '-', '=', '|', '{', '}', '.', '!'];
 
-    public static Task SendMdTextMessage(this ITelegramBotClient botClient,
+    public static Task SendMdTextMessage(
+        this ITelegramBotClient botClient,
         ChatId chatId,
         string text,
         int? messageThreadId = default,
@@ -32,6 +33,30 @@ public static class ClientExtensions
             protectContent: protectContent,
             replyToMessageId: replyToMessageId,
             allowSendingWithoutReply: allowSendingWithoutReply,
+            replyMarkup: replyMarkup,
+            cancellationToken: cancellationToken
+        );
+    }
+
+    public static Task<Message> EditMdMessageText(
+        this ITelegramBotClient botClient,
+        ChatId chatId,
+        int messageId,
+        string text,
+        ParseMode? parseMode = default,
+        IEnumerable<MessageEntity>? entities = default,
+        bool? disableWebPagePreview = default,
+        InlineKeyboardMarkup? replyMarkup = default,
+        CancellationToken cancellationToken = default
+    )
+    {
+        return botClient.EditMessageTextAsync(
+            chatId,
+            messageId,
+            text.EscapeMarkdownSpecialCharacters(),
+            parseMode: ParseMode.MarkdownV2,
+            entities: entities,
+            disableWebPagePreview: disableWebPagePreview,
             replyMarkup: replyMarkup,
             cancellationToken: cancellationToken
         );
--- src/TgBotLib.Core/Utils/Extensions/UpdateExtensions.cs
+++ src/TgBotLib.Core/Utils/Extensions/UpdateExtensions.cs
@@ -13,4 +13,8 @@ public static class UpdateExtensions
     public static long GetChatId(this Update update) => update?.Message?.Chat.Id
                                                         ?? update?.CallbackQuery?.Message?.Chat?.Id
                                                         ?? 0;
+
+    public static string GetInputText(this Update update) => update?.Message?.Text
+                                                             ?? update?.CallbackQuery?.Data
+                                                             ?? "";
 }
